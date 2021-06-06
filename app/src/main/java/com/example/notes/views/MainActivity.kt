35c8dac54bde52921325d92c91d1f5339e671285
Shinkyuuu <codyspark@gmail.com>
2021-06-06 16:08:38 -0700

    Implemented some fragment transition animations

--- app/src/main/java/com/example/notes/views/MainActivity.kt
+++ app/src/main/java/com/example/notes/views/MainActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import com.example.notes.R
+import com.example.notes.views.fragments.MainScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
